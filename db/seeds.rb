1b284f9e19b8815929019da30d2e9b68613fda51
kochapas <kochapas@outlook.com>
2021-08-16 20:45:23 +0900

    Seed file completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,32 @@
 
 puts "🔫 Destroy everything ..."
 User.destroy_all
-Chef_profile.destroy_all
+ChefProfile.destroy_all
 Course.destroy_all
 Booking.destroy_all
 
 puts "🧑 Seed users ..."
-puts "👨‍🍳 Seed chef_profiles ..."
+puts "🔪 Seed chef_profiles ..."
 puts "🍝 Seed courses ..."
 puts "📑 Seed bookings ..."
 
 5.times do |i|
-  user = User.new :name => Faker::Name.name, :email => "test#{i}@email.com", :password => '123456', :password_confirmation => '123456'
+  user = User.new email: "test#{i}@email.com", password: '123456', password_confirmation: '123456'
   user.save
   puts user
   if i.even?
-    chef_profile = Chef_profile.new :user => user, :years_exp => i
+    chef_profile = ChefProfile.new user: user, years_exp: i
     chef_profile.save
     puts chef_profile
     3.times do |j|
-      course = Course.new :name => "#{Faker::Restaurant.type} #{Faker::Food.dish}", :description => Faker::Restaurant.description,
-                          :cuisine_type => rand(5), chef_profile: => chef_profile, duration: => (1..3).sample * 3600000,
-                          price: (3..5).sample * 1000
+      course = Course.new name: "#{Faker::Restaurant.type} #{Faker::Food.dish}", description: Faker::Restaurant.description,
+                          cuisine_type: rand(5), chef_profile: chef_profile, duration: rand(1..3) * 3600000,
+                          price: rand(3..5) * 1000
       course.save
       puts course
       if rand(2).even?
-        booking = Booking.new :course => course, :user => user, :date => Date.today + rand(5),
-                              :time_slot => (1..3).sample, :status:time_slot => (0..3).sample
+        booking = Booking.new course: course, user: user, date: Date.today + rand(5),
+                              time_slot: rand(1..3), status: rand(0..3)
         booking.save
         puts booking
       end
@@ -41,4 +41,4 @@ puts "📑 Seed bookings ..."
   end
 end
 
-puts "🌲 Seed complete ..."
+puts "🌲 Seed complete ... #{User.count} Users / #{ChefProfile.count} ChefProfiles / #{Course.count} Courses / #{Booking.count} Bookings"
