62d63906e7c50c821580e60415e95657cc7ebc85
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-17 15:08:52 +0900

    add the courses_booked and courses_created custom associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :chef_profile
   has_many :bookings
+  # Courses booked as a customer (i.e. to eat)
+  has_many :courses_booked, through: :bookings, source: :course
+  # Courses created as a chef (i.e. to serve)
+  has_many :courses_created, through: :chef_profile, source: :courses
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
