3d9dddfcf0ce8815950e54f561f533f3bc5332be
kochapas <kochapas@outlook.com>
2021-08-17 12:17:14 +0900

    Improve seeds amount, fix problem where booking seed book the course by the user that is the chef himself.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,30 +15,35 @@ Booking.destroy_all
 puts "🧑 Seed users ..."
 puts "🔪 Seed chef_profiles ..."
 puts "🍝 Seed courses ..."
-puts "📑 Seed bookings ..."
 
-5.times do |i|
+10.times do |i|
   user = User.new email: "test#{i}@email.com", password: '123456', password_confirmation: '123456'
-  user.save
-  puts user
+  user.save!
   if i.even?
     chef_profile = ChefProfile.new user: user, years_exp: i
-    chef_profile.save
-    puts chef_profile
-    3.times do |j|
+    chef_profile.save!
+    rand(4..6).times do |j|
       course = Course.new name: "#{Faker::Restaurant.type} #{Faker::Food.dish}", description: Faker::Restaurant.description,
-                          cuisine_type: rand(5), chef_profile: chef_profile, duration: rand(1..3) * 3600000,
-                          price: rand(3..5) * 1000
-      course.save
-      puts course
-      if rand(2).even?
-        booking = Booking.new course: course, user: user, date: Date.today + rand(5),
-                              time_slot: rand(1..3), status: rand(0..3)
-        booking.save
-        puts booking
-      end
+      cuisine_type: rand(5), chef_profile: chef_profile, duration: rand(1..3) * 3600000,
+      price: rand(3..5) * 1000
+      course.save!
     end
   end
+end
+
+puts "📑 Seed bookings ..."
+all_user = User.all
+all_chef = ChefProfile.all
+30.times do |i|
+  user = all_user.sample
+  chef = all_chef.sample
+  while user == chef
+    chef = all_chef.sample
+  end
+  course = chef.courses.sample
+  booking = Booking.new course: course, user: user, date: Date.today + rand(10),
+                        time_slot: rand(1..3), status: rand(0..3)
+  booking.save!
 end
 
 puts "🌲 Seed complete ... #{User.count} Users / #{ChefProfile.count} ChefProfiles / #{Course.count} Courses / #{Booking.count} Bookings"
