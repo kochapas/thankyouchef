416e598e792aeff256ccc74aadb713fb0f4e4b90
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-17 20:22:56 +0700

    added the search ability

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,7 @@ class CoursesController < ApplicationController
   before_action :set_course, only: [:show]
   def index
     @courses = policy_scope(Course).order(created_at: :desc)
+    @courses = @courses.search(params[:query]).order('created_at DESC')
   end
 
   def show
@@ -13,6 +14,8 @@ class CoursesController < ApplicationController
 
   def create
   end
+
+
 
   private
   # Use callbacks to share common setup or constraints between actions.
