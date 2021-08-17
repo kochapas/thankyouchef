3ce341cb04932b5eb41437d59ab68fac6a487638
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-17 13:38:26 +0900

    validates name, description, cuisine_type etc.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,9 @@ class Course < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :chef_profile, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :cuisine_type, presence: true
+  validates :duration, presence: true
+  validates :price, presence: true
 end
