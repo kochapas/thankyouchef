49706728e33570f48e2895cf975c359ba2df65d9
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-17 12:42:09 +0900

    add chef_profile and bookings associations and validates email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :chef_profile
+  has_many :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
 end
