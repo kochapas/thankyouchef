755445fd7c473141cd07b3dfc0c5f2f19f9d63f2
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-19 13:56:38 +0700

    search for multiple tables

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   # belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  has_one :user, through: :chef_profile
 
   validates :chef_profile, presence: true
   validates :name, presence: true
@@ -16,11 +17,18 @@ class Course < ApplicationRecord
     # duration
       against: [ :name, :description , :price],
       associated_against: {
-        user: [ :city, :address ]
+        chef_profile: [ :years_exp ],
+        user: [:first_name, :city]
       },
       using: {
         tsearch: { prefix: true } # <-- now `superman batm` will return something!
       }
+      # associated_against: {
+      #   users: [:first_name]
+      # }
+
+  # include PgSearch::Model
+  # multisearchable against: [ :name, :description , :price]
 
   def self.search(keywords)
     if keywords
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_100423) do
+ActiveRecord::Schema.define(version: 2021_08_19_055450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.date "date"
@@ -44,7 +65,17 @@ ActiveRecord::Schema.define(version: 2021_08_18_100423) do
     t.bigint "chef_profile_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
     t.index ["chef_profile_id"], name: "index_courses_on_chef_profile_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,10 +88,14 @@ ActiveRecord::Schema.define(version: 2021_08_18_100423) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "avatar_url"
+    t.string "city"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "courses"
   add_foreign_key "bookings", "users"
   add_foreign_key "chef_profiles", "users"
