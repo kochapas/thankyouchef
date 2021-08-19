cb137d326be136e343f3129468776f4cdd269244
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-19 15:09:33 +0700

    a start to geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_073207) do
+ActiveRecord::Schema.define(version: 2021_08_19_075647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_073207) do
     t.string "avatar_url"
     t.string "city"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
