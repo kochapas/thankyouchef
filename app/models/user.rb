5fbfb24c290955ad7b02cf8870dc666e8585515c
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-19 18:25:04 +0900

    change user model to accept an image attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :chef_profile
+  has_one_attached :photo
   has_many :bookings
   # Courses booked as a customer (i.e. to eat)
   has_many :courses_booked, through: :bookings, source: :course
