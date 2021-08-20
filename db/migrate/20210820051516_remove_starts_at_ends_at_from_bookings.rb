ee2d128e4f32d2f015ab397d4239be6a15f16824
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-20 15:13:11 +0900

    remove unecessary migration

--- db/migrate/20210820051516_remove_starts_at_ends_at_from_bookings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveStartsAtEndsAtFromBookings < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :bookings, :starts_at, :string
-    remove_column :bookings, :ends_at, :string
-  end
-end
